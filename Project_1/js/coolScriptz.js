44b4efcd259a0fe25509b6a3dbe4d1c482e0286e
j3900 <no>
2014-04-11 17:45:18 -0400

    Added tag search functionality!

--- Project_1/js/coolScriptz.js
+++ Project_1/js/coolScriptz.js
@@ -1,12 +1,23 @@
-
-constructTagQueryLinks();
-
-function constructTagQueryLinks() {
-	var tags = document.getElementsByClassName('tag');
-	
-	for(var i = 0; i < tags.length; i++) {
-		var name = tags[i].innerHTML.subString(1);
-		var url = '../tagquery.html';
-		tags[i].href = url;
+function tagDisplay(goalTag) {
+	var posts = document.getElementsByClassName('post');
+	for(var i = 0; i < posts.length; i++) {
+		var children = posts[i].childNodes;
+		var foundTag = false;
+		for(var j = 0; j < children.length; j++) {
+			if(children[j].innerHTML == goalTag) {
+				foundTag = true;
+				break;
+			}
+		}
+		if(!foundTag) {
+			posts[i].style.display='none';
+		}
 	}
+	
+	var searchResultIndicator = document.getElementById('searchResult');
+	searchResultIndicator.innerHTML = 'Displaying posts tagged: '+goalTag+'. Click me to go back.';
+}
+
+function refreshPage() {
+	location.reload();
 }
