7eff744755b1f31d4dda47a1928583689f8c22e4
j3900 <no>
2014-04-10 22:44:21 -0400

    query prep?

--- Project_1/js/coolScriptz.js
+++ Project_1/js/coolScriptz.js
@@ -1,1 +1,12 @@
 
+constructTagQueryLinks();
+
+function constructTagQueryLinks() {
+	var tags = document.getElementsByClassName('tag');
+	
+	for(var i = 0; i < tags.length; i++) {
+		var name = tags[i].innerHTML.subString(1);
+		var url = '../tagquery.html';
+		tags[i].href = url;
+	}
+}
